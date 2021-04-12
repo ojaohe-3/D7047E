159bee5d3410ea943e2e412d81973becc44d47ce
ojaohe-3 <ojaohe-3@student.ltu.se>
2021-04-12 10:16:33 +0200

    pics

--- modelCNN.py
+++ modelCNN.py
@@ -24,7 +24,7 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=4,
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-
+writer = SummaryWriter()
 
 # functions to show an image
 
@@ -39,27 +39,34 @@ def imshow(img):
 # get some random training images
 
 
-        
+# leakyRelU
 class Network (nn.Module):
     def __init__(self):
         super(Network, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
+        # self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
     def forward(self, x):
-        x = self.pool(F.leaky_relu(self.conv1(x)))
-        x = self.pool(F.leaky_relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        # x = self.pool(F.leaky_relu(self.conv1(x)))
+        x = F.leaky_relu(self.conv1(x))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+        x = F.leaky_relu(self.conv2(x))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+
+        # x = x.view(-1, 16 * 5 * 5)
+        x = torch.flatten(x,start_dim = 1)
         x = F.leaky_relu(self.fc1(x))
         x = F.leaky_relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+# Tanh
 class Network1 (nn.Module):
     def __init__(self):
-        super(Network, self).__init__()
+        super(Network1, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
@@ -67,19 +74,26 @@ class Network1 (nn.Module):
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
     def forward(self, x):
-        x = self.pool(F.tanh(self.conv1(x)))
-        x = self.pool(F.tanh(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        # x = self.pool(F.tanh(self.conv1(x)))
+        # x = self.pool(F.tanh(self.conv2(x)))
+        x = F.tanh(self.conv1(x))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+        x = F.tanh(self.conv2(x))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+
+        # x = x.view(-1, 16 * 5 * 5)
+        x = torch.flatten(x,start_dim = 1)
         x = F.tanh(self.fc1(x))
         x = F.tanh(self.fc2(x))
         x = self.fc3(x)
         return x
 
 
-def train(net, optimizer):
+def train(net, optimizer, title):
     criterion = nn.CrossEntropyLoss()
     for epoch in range(10):  
         running_loss = 0.0
+        running_correct = 0.0
         for i, data in enumerate(trainloader, 0):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data[0].to(device), data[1].to(device)
@@ -95,10 +109,16 @@ def train(net, optimizer):
 
             # print statistics
             running_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1)
+            running_correct += (predicted == labels).sum().item()
             if i % 2000 == 1999:    # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
                     (epoch + 1, i + 1, running_loss / 2000))
+                writer.add_scalar('training loss, ' + title, running_loss/2000, epoch)
+                writer.add_scalar('accuracy, '+title, running_correct/2000, epoch)
+                
                 running_loss = 0.0
+                running_correct = 0.0
 
     print('Finished Training')
 
@@ -119,37 +139,38 @@ def validate(net):
 
 
 if __name__ == '__main__':
-    # dataiter = iter(trainloader)
-    # images, labels = dataiter.next()
-
-    # show images
-    # imshow(torchvision.utils.make_grid(images))
-    # print labels
-    # print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     net = Network()
-    net.to(device)
     net1 = Network1()
+    
+    #==== Loading tensorboard with the models
+    data = iter(trainloader)
+    example, _ = data.next()
+    writer.add_graph(net, example)
+    writer.add_graph(net1, example)
+
     net.to(device)
+    net1.to(device)
 
 
+    #==== running test with leaky ReLU ====
     optimizer = optim.SGD(net.parameters(), lr=0.0001)
-    train(net, optimizer)
+    train(net, optimizer, "SGD LeakyReLu")
     print("with optimizer SGD and Leaky ReLu:")
     validate(net)
     
     optimizer = optim.Adam(net.parameters(), lr=0.0001)
-    train(net, optimizer)
+    train(net, optimizer, "Adam LeakyReLu")
     print("with optimizer Adam and Leaky ReLu:")
     validate(net)
 
     # ==== With tanh ====
     optimizer = optim.SGD(net.parameters(), lr=0.0001)
-    train(net, optimizer)
+    train(net, optimizer, "SGD tanh")
     print("with optimizer SGD and tanh:")
     validate(net)
     
     optimizer = optim.Adam(net.parameters(), lr=0.0001)
-    train(net, optimizer)
+    train(net, optimizer, "Adam tanh")
     print("with optimizer Adam and tanh:")
     validate(net)
