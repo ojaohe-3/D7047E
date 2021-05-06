cc920151d953d608e5001e781628b40401766b1d
Johan holmgren <ojaohe-3>
2021-05-06 10:06:30 +0200

    project lungs 5 epochs 60 - 80 % acc, 20 epochs <80%

---
+++ Visualizer.py
@@ -0,0 +1,10 @@
+import cv2
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, models, transforms
+import numpy as np
+
+
